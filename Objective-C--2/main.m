04a87831c81332c3a11fe8b21004171324c02c22
yatakan <takaya423@outlook.jp>
2017-09-17 18:46:12 +0900

    Initial Commit

---
+++ Objective-C--2/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Objective-C--2
+//
+//  Created by taka on 2017/09/17.
+//  Copyright © 2017年 takayan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
